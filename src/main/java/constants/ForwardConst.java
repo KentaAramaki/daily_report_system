f45ffa0d5f39e68385ee5d459889416ac146d52b
kenta.aramaki <mgk125ap@gmail.com>
2022-10-02 20:31:16 +0900

    従業員showアクションの作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -4,14 +4,14 @@ package constants;
  */
 public enum ForwardConst {
 
-    // action
+  //action
     ACT("action"),
     ACT_TOP("Top"),
     ACT_EMP("Employee"),
     ACT_REP("Report"),
     ACT_AUTH("Auth"),
 
-    // command
+    //command
     CMD("command"),
     CMD_NONE(""),
     CMD_INDEX("index"),
@@ -21,10 +21,11 @@ public enum ForwardConst {
     CMD_LOGOUT("logout"),
     CMD_NEW("entryNew"),
     CMD_CREATE("create"),
+    CMD_EDIT("edit"),
     CMD_UPDATE("update"),
     CMD_DESTROY("destroy"),
 
-    // jsp
+    //jsp
     FW_ERR_UNKNOWN("error/unknown"),
     FW_TOP_INDEX("topPage/index"),
     FW_LOGIN("login/login"),
@@ -37,24 +38,23 @@ public enum ForwardConst {
     FW_REP_NEW("reports/new"),
     FW_REP_EDIT("reports/edit");
 
-    /*
+    /**
      * 文字列
      */
     private final String text;
 
-    /*
+    /**
      * コンストラクタ
      */
     private ForwardConst(final String text) {
         this.text = text;
     }
 
-    /*
-     * 値（文字列）取得
+    /**
+     * 値(文字列)取得
      */
     public String getValue() {
         return this.text;
     }
-
 
 }
